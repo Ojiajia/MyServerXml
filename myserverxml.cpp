437c4fc8118d097aa8b8673dbda96c1d0aa43636
Ojiajia <18286381+Ojiajia@users.noreply.github.com>
2019-03-01 17:34:40 +0300

    Update myserverxml.cpp

--- myserverxml.cpp
+++ myserverxml.cpp
@@ -7,7 +7,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <QListIterator>
-#include <QtXml>
+
 #include <QDebug>
 #include <QtXml>
 
@@ -26,7 +26,7 @@ MyServer::MyServer(int nPort, QWidget* pwgt /*=0*/) : QWidget(pwgt)
                               );
         m_ptcpServer->close();
         return;
-    }
+   }
     connect(m_ptcpServer, SIGNAL(newConnection()),
     this,                 SLOT(slotNewConnection())
     );
@@ -45,7 +45,7 @@ MyServer::MyServer(int nPort, QWidget* pwgt /*=0*/) : QWidget(pwgt)
     pvbxLayout->addWidget(new QLabel("<H1>Server</H1>"));
     pvbxLayout->addWidget(m_ptxt);
     setLayout(pvbxLayout);
-    qDebug() << "MyServer class works";
+//    qDebug() << "MyServer class works";
 }
 
 //***********************************************************************
@@ -60,7 +60,7 @@ MyServer::MyServer(int nPort, QWidget* pwgt /*=0*/) : QWidget(pwgt)
 
 
 
-    qDebug() << "slotNewConnection works";
+//    qDebug() << "slotNewConnection works";
 
     connect(pClientSocket, SIGNAL(disconnected()), //standart
             pClientSocket, SLOT(deleteLater())     //standart
@@ -95,41 +95,153 @@ void MyServer::slotReadClient()
          QTime   time;
          // переменная in - объект потока QDataStream
          in >> str; // получение <xml>
-         //QString strMessage = str;
-         qDebug() << "msg in <xml>: " << str;
+
+         qDebug() << "msg from client " << str;
+
          QDomDocument doc;
          doc.setContent(str);
          QDomElement domElement = doc.documentElement();
 
          QDomNode domNode = domElement.firstChild();
          while(!domNode.isNull()) {
-         traverseNode(domNode);
+         traverseNode(domNode, str2);
          }
 
-         qDebug() << "after traverseNode";
-         m_ptxt->append(str);
+//         qDebug() << "str2 after traverseNode: " << str2;
+        // str2 - ник из хмл
+
+//        usersList.insert(pClientSocket* , str2);
+
+
+
+
+//       вывод ника (.value из QMap)
+//         if (usersList.contains(pClientSocket)) {
+
+//             qDebug() << "slotReadClient" << usersList.value(pClientSocket);
+//         }
+
+//         qDebug() << "begin of users list:";
+
+         QMap<QTcpSocket*,QString>::const_iterator i = usersList.constBegin();
+
+
+
+//         while (i != usersList.constEnd()) {
+
+
+
+//             i.key() = pClientSocket;
+//             i.value() = str2;
+
+////             sendToClient(i.key(),str);
+
+//             ++i;
+
+//         }
+
+//         i = 0;
+
+         usersList[pClientSocket] = str2;
+
+         int intMsgType = 3;
+
+         QString strMsgType = QString::number(intMsgType);
+
+         QString msgToClient = "<data><type>" + strMsgType + "</type>";
+
+//         qDebug() << msgToClient;
+
+//       условное количество клиентов
+
+         int clientId;
+
+
+//         qDebug() << usersList.size();
+       for (int clientId = 1; clientId <= usersList.size(); clientId++) {
+
+//       for (int clientId = 1; clientId <= usersList.size(); clientId++) {
+
+
+
+//         printf();
+//         << " usersList.constEnd() = " << usersList.constEnd();
+
+              while (i != usersList.constEnd()) {
+
+//                     qDebug() << "client id = " << clientId;
+
+                     QString clientIdStr = QString::number(clientId);
+
+                      msgToClient = msgToClient + "<client id = \"" + clientIdStr + "\">" + i.value() + "</client>";
+
+//                     qDebug() << " msg ends with </client> " << msgToClient;
+
+                     ++i;
+
+             }
+
+             i = 0;
+
+         }
+
+         clientId = 0;
+
+         msgToClient = msgToClient + "</data>";
+
+//         qDebug() << " ends up with </data> " << msgToClient;
+
+//         i = usersList.constBegin();
+
+
+
+         while (i != usersList.constEnd()) {
+
+                sendToClient(i.key(),msgToClient);
+
+                ++i;
+
+               }
+
+         i = 0;
+
+         qDebug() << "msg sent";
+
+         //m_ptxt->append(str);
 
          m_nNextBlockSize = 0;
-         // сообщение клиенту о прочтении данных
-//         sendToClient(pClientSocket,"Server Response: " + str2, " sent: " + str1);
+
      }
 }
 
 //***********************************************************************
 
-void MyServer::traverseNode(QDomNode& domNode)
+//***********************************************************************
+
+QString MyServer::traverseNode(QDomNode& domNode,QString& str2)
 {
 
         if(domNode.isElement()) {
            QDomElement domElement = domNode.toElement();
            if(!domElement.isNull()) {
-                qDebug() << "TagName: " << domElement.tagName();
-                qDebug() << "Text: "    << domElement.text();
+//                qDebug() << "TagName: " << domElement.tagName();
+//                qDebug() << "Text: "    << domElement.text();
+           if(domElement.tagName() == "nickname") {
+//                qDebug() << "nickname found: " << domElement.text();
+                str2 = domElement.text();
+//                qDebug() << "str2 from traverseNode: " << str2;
+//                usersList.insert(pClientSocket, str2);
+//                if (usersList.contains(str2)) {
+//                    qDebug() << "slotReadClient" << usersList.value(str2);
+//                }
            }
+           }
+
         }
 
 domNode = domNode.nextSibling();
 //traverseNode(domNode);
+return str2; // str2 - ник из хмл
 }
 
 //***********************************************************************
@@ -139,7 +251,7 @@ domNode = domNode.nextSibling();
 // заранее неизвестен размер блока -> нельзя сразу записать данные
 // в сокет, т.к. размер блока должен быть записан в первую очередь
 
-void MyServer::sendToClient(QTcpSocket* pSocket, QString str)
+void MyServer::sendToClient(QTcpSocket* pSocket,QString& str)
  {
     QByteArray  arrBlock; // массив байтов
     QDataStream out(&arrBlock, QIODevice::WriteOnly);
@@ -154,7 +266,7 @@ void MyServer::sendToClient(QTcpSocket* pSocket, QString str)
 
     out.setVersion(QDataStream::Qt_5_3);
 
-    out << quint16(0) << QTime::currentTime() << str;
+    out << quint16(0) << str;
     out.device()->seek(0); // перемещение указателя на начало блока
     out << quint16(arrBlock.size() - sizeof(quint16));
     // arrBlock.size() - размер блока - 2 байта,+запись в поток out
